5209198d5d4c7a0e0be913671f19a5700a493d79
Eduardo Bray <ejbray@uc.cl>
2026-02-09 22:15:33 -0300

    style: apply black formatter

--- coffee_program_Callealta.py
+++ coffee_program_Callealta.py
@@ -1,4 +1,4 @@
-#data program
+# data program
 
 MENU = {
     "espresso": {
@@ -23,7 +23,7 @@ MENU = {
             "coffee": 24,
         },
         "cost": 3.0,
-    }
+    },
 }
 
 resources = {
@@ -32,7 +32,7 @@ resources = {
     "coffee": 100,
 }
 
-#messagges
+# messagges
 
 order_done_msj = "The drink is dispensed, Thanks"
 order_options = "What would you like? (espresso/latte/cappuccino)"
@@ -42,24 +42,38 @@ string_quarters = "quarters"
 string_dimes = "dimes"
 string_nickels = "nickels"
 string_pennies = "pennies"
-list_money_amounts_strings = [string_quarters, string_dimes, string_nickels, string_pennies]
+list_money_amounts_strings = [
+    string_quarters,
+    string_dimes,
+    string_nickels,
+    string_pennies,
+]
 
-messages = [order_done_msj, order_options, turn_off, h_short,
-            string_quarters, string_dimes, string_pennies,
-            list_money_amounts_strings]
-#Variables
+messages = [
+    order_done_msj,
+    order_options,
+    turn_off,
+    h_short,
+    string_quarters,
+    string_dimes,
+    string_pennies,
+    list_money_amounts_strings,
+]
+# Variables
 cash = 0
 
-#Defs
+
+# Defs
 def prompting_well(prompting):
     """
     prompting for actions you can do
     :param prompting: words allows
     :return: Strings with correct word
     """
-    while prompting not in ["espresso", "latte", "cappuccino", "off", "report" ]:
+    while prompting not in ["espresso", "latte", "cappuccino", "off", "report"]:
         prompting = input(f"choose one\n{order_options[21:]}: ").lower()
     return prompting
+
 
 def off(secret_word):
     """
@@ -71,16 +85,18 @@ def off(secret_word):
         print(turn_off)
         exit()
 
+
 def report(money):
     """Full report:
     program give measures regard the Menu"""
 
     count = 0
     for key in resources:
-        measure = ["ml", "ml","g"]
+        measure = ["ml", "ml", "g"]
         print(f"{key}: {resources[key]} {measure[count]}")
-        count +=1
+        count += 1
     print(f"Money: ${money}")
+
 
 def checking_integrates(number_or_not, amount_in_string):
     """
@@ -91,11 +107,14 @@ def checking_integrates(number_or_not, amount_in_string):
     """
 
     while not number_or_not.isdigit():
-        number_or_not = input(f"Please write it well.\n{h_short} {amount_in_string}: ?: ")
-    amounts =  {amount_in_string :number_or_not}
+        number_or_not = input(
+            f"Please write it well.\n{h_short} {amount_in_string}: ?: "
+        )
+    amounts = {amount_in_string: number_or_not}
     return amounts
 
-def money_user_dict() -> list: #with dict
+
+def money_user_dict() -> list:  # with dict
     """
     checking how much money user put into machine
     :return: dictionary with info
@@ -104,14 +123,19 @@ def money_user_dict() -> list: #with dict
     counting = 0
     user_cash = []
     while True:
-        money_user = input(f"{h_short} {list_money_amounts_strings[counting]}?: ")  # h = how many?
-        dict_info_money = checking_integrates(money_user, list_money_amounts_strings[counting])
+        money_user = input(
+            f"{h_short} {list_money_amounts_strings[counting]}?: "
+        )  # h = how many?
+        dict_info_money = checking_integrates(
+            money_user, list_money_amounts_strings[counting]
+        )
         user_cash.append(dict_info_money)
         print(user_cash)
-        counting +=1
+        counting += 1
 
         if counting == len(list_money_amounts_strings):
             return user_cash
+
 
 def how_much_money_user_put(how_much) -> float:
 
@@ -122,6 +146,7 @@ def how_much_money_user_put(how_much) -> float:
     calculating += float(how_much[3]["pennies"]) * 0.01
 
     return calculating
+
 
 def cost_product_resources_updates(product, users_cash):
     """This calculates how much change you receive
@@ -149,25 +174,29 @@ def cost_product_resources_updates(product, users_cash):
         resources["coffee"] = resources["coffee"] - coffee
         return change
 
+
 def enough_resources(resources_to_check: dict, prompt_product: str):
     measure = ["ml", "ml", "g"]
     counting = 0
     for items in resources_to_check:
 
+        if resources_to_check[items] > resources[items]:
 
-            if resources_to_check[items] > resources[items]:
+            print(
+                f"not enough {items}: {resources[items]} {measure[counting]}.\nIt needs to be refilled"
+            )
+        counting += 1
 
-                print(f"not enough {items}: {resources[items]} {measure[counting]}.\nIt needs to be refilled")
-            counting +=1
 
-#def to run program
+# def to run program
+
 
 def running():
     going = True
     while going:
         prompt = prompting_well(input(f"{order_options}: ").lower())
         if prompt == "off":
-            off(prompt) #checking out if they want to turn off
+            off(prompt)  # checking out if they want to turn off
         elif prompt == "report":
             report(cash)
             running()
@@ -178,9 +207,13 @@ def running():
         money_float_user_total = how_much_money_user_put(user_amount)
         your_change = cost_product_resources_updates(prompt, money_float_user_total)
         if your_change < 0:
-            print(f"you dont have enough money, money being refund it ${round(how_much_money_user_put(user_amount),2)}")
+            print(
+                f"you dont have enough money, money being refund it ${round(how_much_money_user_put(user_amount),2)}"
+            )
             running()
-        print(f"your change of {prompt} which cost ${MENU[prompt]["cost"]} is ${round(your_change,2)}")
+        print(
+            f"your change of {prompt} which cost ${MENU[prompt]["cost"]} is ${round(your_change,2)}"
+        )
         enough_resources(MENU[prompt]["ingredients"], prompt)
 
         should_continue = input("Do you want to continue? [y/n]").lower()
@@ -191,15 +224,10 @@ def running():
         else:
             going = True
 
-#trying program ;D
+
+# trying program ;D
 running()
 
-#All code made by Rodrigo Callealta except data giving by teacher Angela from Udemy
-#Download the PDF
+# All code made by Rodrigo Callealta except data giving by teacher Angela from Udemy
+# Download the PDF
 # for the  program requirements here: https://drive.google.com/uc?export=download&id=1eIZt2TeFGVrk4nXkx8E_5Slw2coEcOUo
-
-
-
-
-
-
